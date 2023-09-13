a0754d517ceb68e86c6d83547db65277dbcf8d5d
LaodingCanoe <iskanderlife2005@gmail.com>
2023-09-14 00:26:29 +0500

    Бюрузовый кнопка закрыть закрыта

--- Second/Form1.Designer.cs
+++ Second/Form1.Designer.cs
@@ -29,27 +29,14 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Second));
-            this.ClOsE = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // ClOsE
-            // 
-            this.ClOsE.BackColor = System.Drawing.Color.Silver;
-            this.ClOsE.Location = new System.Drawing.Point(73, 279);
-            this.ClOsE.Name = "ClOsE";
-            this.ClOsE.Size = new System.Drawing.Size(153, 55);
-            this.ClOsE.TabIndex = 0;
-            this.ClOsE.Text = "ClOsE";
-            this.ClOsE.UseVisualStyleBackColor = false;
-            this.ClOsE.Click += new System.EventHandler(this.ClOsE_Click);
             // 
             // Second
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(320, 642);
-            this.Controls.Add(this.ClOsE);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Second";
             this.Text = "Second";
@@ -59,8 +46,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button ClOsE;
     }
 }
 
